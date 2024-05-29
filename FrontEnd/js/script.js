bbb7d205f18f48878eff4504798e2af02ccb60d2
Valentin <65048905+vdhilly@users.noreply.github.com>
2024-05-29 13:25:50 +0200

    filtre

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,22 +1,3 @@
-function createCategoryBtn(title, categoryId) {
-  const container = document.querySelector(".gallery-categories");
-  const Btn = document.createElement("button");
-  Btn.innerText = title;
-  Btn.dataset.categoryId = categoryId;
-  Btn.classList.add("category-btn");
-  container.appendChild(Btn);
-}
-
-// fetch categories
-const categories = fetch("http://localhost:5678/api/categories")
-  .then((response) => response.json())
-  .then((categories) =>
-    categories.forEach((category, index) => {
-      if (index === 0) createCategoryBtn("Tous", "");
-      createCategoryBtn(category.name, category.id);
-    })
-  );
-
 // fetch pour aller chercher les travaux
 const works = fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
@@ -31,9 +12,42 @@ const works = fetch("http://localhost:5678/api/works")
 
       const figure = document.createElement("figure");
       figure.append(img, figcaption);
+      figure.classList.add("work");
+      figure.dataset.category = element.categoryId;
 
       const gallery = document.querySelector(".gallery");
 
       gallery.appendChild(figure);
     })
   );
+
+function createCategoryBtn(title, categoryId) {
+  const container = document.querySelector(".gallery-categories");
+  const Btn = document.createElement("button");
+  Btn.innerText = title;
+  Btn.dataset.categoryId = categoryId;
+  Btn.classList.add("category-btn");
+
+  Btn.addEventListener("click", () => handleFilters(categoryId));
+  container.appendChild(Btn);
+}
+
+// fetch categories
+const categories = fetch("http://localhost:5678/api/categories")
+  .then((response) => response.json())
+  .then((categories) =>
+    categories.forEach((category, index) => {
+      if (index === 0) createCategoryBtn("Tous", "");
+      createCategoryBtn(category.name, category.id);
+    })
+  );
+
+function handleFilters(categoryId) {
+  const works = document.querySelectorAll(".work");
+  works.forEach((work) => {
+    const category = Number(work.dataset.category);
+    if (category === categoryId) work.style.display = "block";
+    else if (categoryId === "") work.style.display = "block";
+    else work.style.display = "none";
+  });
+}
